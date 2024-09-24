656282bf52f9ba5ba38d90ffc5c79c0d6fdfe49a
Lee <lee@mtftm.com>
2024-09-24 20:45:11 +0800

    Optimize site configuration by updating import statement and site title/logo settings in CopyrightInfo.vue

--- src/components/CopyrightInfo.vue
+++ src/components/CopyrightInfo.vue
@@ -6,33 +6,40 @@ import { ref, watch } from 'vue'
 const { frontmatter, attrs } = useData()
 
 // 响应式变量
-const enableCopyright = ref(attrs?.copyright?.enable ?? false)
-
-// 检查是否至少有一个作者
-const hasAuthors = ref(attrs?.author?.length > 0)
+const enableCopyright = ref(frontmatter.value.copyright?.enable ?? false)
+const originUrl = ref(frontmatter.value.copyright?.url ?? '')
+const license = ref(frontmatter.value.copyright?.license ?? '')
+const licenseUrl = ref(frontmatter.value.copyright?.licenseUrl ?? '')
+const authors = ref(attrs?.author ?? [])
 
 // 监听 frontmatter 的变化
-watch(() => frontmatter.value, () => {
-  enableCopyright.value = attrs?.copyright?.enable ?? false
-  hasAuthors.value = attrs?.author?.length > 0
+watch(() => frontmatter.value, (newFrontmatter) => {
+  enableCopyright.value = newFrontmatter.copyright?.enable ?? false
+  originUrl.value = newFrontmatter.copyright?.url ?? ''
+  license.value = newFrontmatter.copyright?.license ?? ''
+  licenseUrl.value = newFrontmatter.copyright?.licenseUrl ?? ''
+  authors.value = attrs?.author ?? []
 })
 
 </script>
 
 <template>
-  <div v-if="enableCopyright && hasAuthors">
+  <div v-if="enableCopyright && authors.length > 0">
     <div class="tip custom-block">
       <p class="custom-block-title">Copyright</p>
       <p>
         <span>这篇文章 </span>
-        <a v-if="frontmatter.originUrl" :href="frontmatter.originUrl">{{ frontmatter.title }}</a>
+        <a v-if="originUrl" :href="originUrl">{{ frontmatter.title }}</a>
         <span v-else>{{ frontmatter.title }}</span>
         <span> 由 </span>
-        <span v-for="author in attrs?.author" :key="author">{{ author }}<span v-if="!$last">、</span></span>
+        <span v-for="(author, index) in authors" :key="author">
+          {{ author }}<span v-if="index !== authors.length - 1">、</span>
+        </span>
         <span> 创作</span>
-        <span v-if="frontmatter.license">
+        <span v-if="license">
           <span>，Project Trans 在 </span>
-          <span>{{ frontmatter.license }}</span>
+          <a v-if="licenseUrl" :href="licenseUrl">{{ license }}</a>
+          <span v-else>{{ license }}</span>
           <span> 许可下使用</span>
         </span>
         <span>。</span>
