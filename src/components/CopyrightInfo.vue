95cc738520c6f9aaa22b810df6d3db5b1278da6c
Lee <lee@mtftm.com>
2024-09-24 19:37:43 +0800

    Optimize site configuration by updating import statement and site title/logo settings in CopyrightInfo.vue

--- src/components/CopyrightInfo.vue
+++ src/components/CopyrightInfo.vue
@@ -33,28 +33,31 @@ const originUrl = ref('javascript:void(0)')
 const licenseUrl = ref('javascript:void(0)')
 const frontmatter = ref(useData().frontmatter.value)
 
-// 监听路径变化
+// 监听路径变化并更新相关变量
 watch(
   () => useData().page.value.relativePath,
   (newPath) => {
     const paths = newPath.replace('.md', '').split('/').filter((item: string) => item !== '')
     attrs.value = searchClosestInTrie(data, paths)
+    
     if (attrs.value) {
       originUrl.value = attrs.value.copyright?.url ?? 'javascript:void(0)'
       licenseUrl.value = attrs.value.copyright?.licenseUrl ?? 'javascript:void(0)'
     }
   },
-  { immediate: true }
+  { immediate: true }  // 初次加载时立即执行一次
 )
 
+// 监听 frontmatter 的变化
 watch(
   () => useData().frontmatter.value,
   (newFrontmatter) => {
     frontmatter.value = newFrontmatter
   },
-  { immediate: true }
+  { immediate: true }  // 初次加载时立即执行一次
 )
 
+// 计算是否存在相关信息
 const originUrlExists = () => (attrs.value?.copyright?.url ?? null) != null
 const licenseExists = () => attrs.value?.copyright?.license != null
 const licenseUrlExists = () => (attrs.value?.copyright?.licenseUrl ?? null) != null
@@ -66,15 +69,15 @@ const licenseUrlExists = () => (attrs.value?.copyright?.licenseUrl ?? null) != n
       <p class="custom-block-title">Copyright</p>
       <p>
         <span>这篇文章 </span>
-        <a v-if="originUrlExists" :href="originUrl">{{ frontmatter.title }}</a>
+        <a v-if="originUrlExists()" :href="originUrl">{{ frontmatter.title }}</a>
         <span v-else>{{ frontmatter.title }}</span>
         <span> 由 </span>
         <span v-for="author in attrs?.author" :key="author">{{ author }}</span>
         <span> 创作</span>
-        <span v-if="licenseExists">
+        <span v-if="licenseExists()">
           <span>，Project Trans 在 </span>
-          <a v-if="licenseUrlExists" :href="licenseUrl">{{ license }}</a>
-          <span v-else>{{ license }}</span>
+          <a v-if="licenseUrlExists()" :href="licenseUrl">{{ attrs.value?.copyright?.license }}</a>
+          <span v-else>{{ attrs.value?.copyright?.license }}</span>
           <span> 许可下使用</span>
         </span>
         <span>。</span>
@@ -83,4 +86,3 @@ const licenseUrlExists = () => (attrs.value?.copyright?.licenseUrl ?? null) != n
     <hr>
   </div>
 </template>
-
