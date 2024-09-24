d497050d115d5d969e459ccb7af18a850e589430
Lee <lee@mtftm.com>
2024-09-24 20:40:16 +0800

    Optimize site configuration by updating import statement and site title/logo settings in CopyrightInfo.vue

--- src/components/CopyrightInfo.vue
+++ src/components/CopyrightInfo.vue
@@ -7,32 +7,32 @@ const { frontmatter, attrs } = useData()
 
 // 响应式变量
 const enableCopyright = ref(attrs?.copyright?.enable ?? false)
-const originUrlExists = ref(Boolean(frontmatter.originUrl))
-const licenseExists = ref(Boolean(frontmatter.license))
-const licenseUrlExists = ref(Boolean(frontmatter.licenseUrl))
+
+// 检查是否至少有一个作者
+const hasAuthors = ref(attrs?.author?.length > 0)
 
 // 监听 frontmatter 的变化
-watch(() => frontmatter.value, (newFrontmatter) => {
+watch(() => frontmatter.value, () => {
   enableCopyright.value = attrs?.copyright?.enable ?? false
+  hasAuthors.value = attrs?.author?.length > 0
 })
 
 </script>
 
 <template>
-  <div v-if="enableCopyright">
+  <div v-if="enableCopyright && hasAuthors">
     <div class="tip custom-block">
       <p class="custom-block-title">Copyright</p>
       <p>
         <span>这篇文章 </span>
-        <a v-if="originUrlExists" :href="frontmatter.originUrl">{{ frontmatter.title }}</a>
+        <a v-if="frontmatter.originUrl" :href="frontmatter.originUrl">{{ frontmatter.title }}</a>
         <span v-else>{{ frontmatter.title }}</span>
         <span> 由 </span>
-        <span v-for="author in attrs?.author" :key="author">{{ author }}</span>
+        <span v-for="author in attrs?.author" :key="author">{{ author }}<span v-if="!$last">、</span></span>
         <span> 创作</span>
-        <span v-if="licenseExists">
+        <span v-if="frontmatter.license">
           <span>，Project Trans 在 </span>
-          <a v-if="licenseUrlExists" :href="frontmatter.licenseUrl">{{ frontmatter.license }}</a>
-          <span v-else>{{ frontmatter.license }}</span>
+          <span>{{ frontmatter.license }}</span>
           <span> 许可下使用</span>
         </span>
         <span>。</span>
