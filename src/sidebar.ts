4d1f33b51dc026724c5af70ab3b85ad33397d97f
Lee <lee@mtftm.com>
2024-10-02 10:52:08 +0800

    优化侧边栏排序逻辑

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -20,6 +20,7 @@ export function generateSidebar() {
 }
 
 function sidebarTitleSorter(infoA: SidebarItem, infoB: SidebarItem): number {
+  
   // 优先根据 order 字段排序
   if (infoA.order !== undefined && infoB.order !== undefined) {
     if (infoA.order !== infoB.order) {
@@ -27,16 +28,19 @@ function sidebarTitleSorter(infoA: SidebarItem, infoB: SidebarItem): number {
     }
   }
 
-  // 如果 order 字段相等或不存在，则根据 text 字段排序
-  // const textA = infoA.text;
-  // const textB = infoB.text;
-  // if (textA === undefined || textB === undefined) {
-  //   return 0;
-  // }
+  //如果 order 字段相等或不存在，则根据 text 字段排序
+  const textA = infoA.text;
+  const textB = infoB.text;
+  if (textA === undefined || textB === undefined) {
+    return 0;
+  }
 
-  // const infoANfc = textA.normalize('NFC');
-  // const infoBNfc = textB.normalize('NFC');
-  // return infoANfc.localeCompare(infoBNfc, 'zh', {
-  //   numeric: true,
-  // });
+  const infoANfc = textA.normalize('NFC');
+  const infoBNfc = textB.normalize('NFC');
+  if (infoA.order == undefined && infoB.order == undefined) {
+    return infoANfc.localeCompare(infoBNfc, 'zh', {
+      numeric: true,
+    });
+  }
+ 
 }
