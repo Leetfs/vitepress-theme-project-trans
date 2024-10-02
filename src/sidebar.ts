cee0bba518529ed9be65c319281e0cea77c768e4
Lee <lee@mtftm.com>
2024-10-02 11:50:34 +0800

    优化侧边栏排序逻辑，根据 text 字段排序，如果 order 字段不存在

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -20,25 +20,29 @@ export function generateSidebar() {
 }
 
 function sidebarTitleSorter(infoA: SidebarItem, infoB: SidebarItem): number {
-  
   // 优先根据 order 字段排序
-  if (infoA.order !== undefined && infoB.order !== undefined) {
+  if (infoA.order !== undefined || infoB.order !== undefined) {
+    // 如果只有一个存在 order，则优先那个
+    if (infoA.order === undefined) return 1;
+    if (infoB.order === undefined) return -1;
+    // 如果两个都存在，则比较它们的值
     if (infoA.order !== infoB.order) {
       return infoA.order - infoB.order;
     }
   }
-  //如果 order 字段不存在，则根据 text 字段排序
-  else {
-    const textA = infoA.text;
-    const textB = infoB.text;
-    const infoANfc = textA.normalize('NFC');
-    const infoBNfc = textB.normalize('NFC');
-    if (textA === undefined || textB === undefined) {
-      return 0;
-    }
-    return infoANfc.localeCompare(infoBNfc, 'zh', {
-      numeric: true,
-    });
+  
+  // 如果 order 字段不存在，则根据 text 字段排序
+  const textA = infoA.text;
+  const textB = infoB.text;
+  
+  if (textA === undefined || textB === undefined) {
+    return 0;
   }
- 
+
+  const infoANfc = textA.normalize('NFC');
+  const infoBNfc = textB.normalize('NFC');
+
+  return infoANfc.localeCompare(infoBNfc, 'zh', {
+    numeric: true,
+  });
 }
